42ac5e6def3aed135a2bda955e6606c46b8e80b9
Mayureshwar Mahadevan Reddy <mayureshreddy2006@gmail.com>
2025-04-03 14:35:05 +0000

    changes in blockstudy.js file

--- blockstudy.js
+++ blockstudy.js
@@ -1,10 +1,10 @@
-const {ethers} = require('ethers');
+const {ethers} = require("ethers");
 
 const provider = new ethers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/ILZFTCIzZpUNxp6ScjhRIDxksNEDd2Ye');
 
-async function getbalance(address) {
-    const balance = await provider.getbalance(address);
-    console.log(ethers.formatEther(balance) , ETH);
+async function getBalance(address) {
+    const balance = await provider.getBalance(address);
+    console.log(ethers.formatEther(balance) , "ETH");
 }
 
-getbalance(0x3F6FECB35e601fD0266653A806c9ae6434EeCcb3)
+getBalance("0x3F6FECB35e601fD0266653A806c9ae6434EeCcb3");
