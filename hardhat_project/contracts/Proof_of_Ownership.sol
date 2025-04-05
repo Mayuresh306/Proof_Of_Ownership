c933c561311e8470f75bab96b778a651e15d28fc
Mayureshwar Mahadevan Reddy <mayureshreddy2006@gmail.com>
2025-04-05 08:23:46 +0000

    changes made

---
+++ hardhat_project/contracts/Proof_of_Ownership.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract ProofOfOwnership {
+    struct Document {
+        string hash;
+        address owner;
+        uint timestamp;
+    }
+
+    mapping(string => Document) private documents;
+
+    event RegisteredDocument(string hash , address owner , uint timestamp);
+
+    function DocumentRegister(string memory _hash) public {
+        require(documents[_hash].timestamp == 0 , "Document already Registered" );
+        documents[_hash] = Document({
+            hash: _hash,
+            owner: msg.sender,
+            timestamp: block.timestamp
+        });
+
+        emit RegisteredDocument(_hash , msg.sender , block.timestamp);
+    }
+
+
+    function VerifyDocument(string memory _hash) public view returns(address , uint) {
+        require(documents[_hash].timestamp != 0 , "Document not Found");
+
+        Document memory doc = documents[_hash];
+        return (doc.owner , doc.timestamp);
+    }
+}
